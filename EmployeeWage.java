21f36550a007972158dffd5eed6d07928eb46b60
sonali2495 <sonaligadge24@gmail.com>
2021-08-21 23:35:29 +0530

    Updated Employee Wage Use Case 5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,24 +4,27 @@ public class EmployeeWage {
 	public static final int IsPartTime = 1;
 	public static final int IsFullTime = 2;
 	public static final int EmpRatePerHour = 20;
+	public static final int numOfWorkingDays = 2;
 
 	public static void main(String[] args) {
-		int empHrs = 0;
-		int empWage = 0;
+		int empHrs = 0, empWage = 0, totalEmpWage = 0;
 
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case IsPartTime:
-			empHrs = 4;
-			break;
-		case IsFullTime:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
+		for (int day = 0; day < numOfWorkingDays; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10 % 3);
+			switch (empCheck) {
+			case IsPartTime:
+				empHrs = 4;
+				break;
+			case IsFullTime:
+				empHrs = 8;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EmpRatePerHour;
+			totalEmpWage += empWage;
+			System.out.println("Employee Wage= " + empWage);
 		}
-		empWage = empHrs * EmpRatePerHour;
-		System.out.println("Employee Wage= " + empWage);
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 
 }
