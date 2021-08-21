e9be4d980280f9a3e67b22104f6d2bb7d7796c69
sonali2495 <sonaligadge24@gmail.com>
2021-08-21 23:20:03 +0530

    Updated Employee Wage Use Case 3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,13 +3,16 @@ package com.bridgelabz.Fellowship_Batch;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		int IsFullTime = 1;
+		int IsPartTime = 1;
+		int IsFullTime = 2;
 		int EmpRatePerHour = 20;
 		int empHrs = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IsFullTime)
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IsPartTime)
+			empHrs = 4;
+		else if (empCheck == IsFullTime)
 			empHrs = 8;
 		else
 			empHrs = 0;
