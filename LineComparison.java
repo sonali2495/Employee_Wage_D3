6843ebaa4d82a325d1f0c01fd467f8876e6c92df
sonali2495 <sonaligadge24@gmail.com>
2021-08-22 09:18:24 +0530

    Welcome to Line Comparison Use Case

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Fellowship_Batch;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation");
+
+	}
+
+}
